c4c0ab7845938b9488e8d2a38fbbd85ce8f41014
charlie  lee <ctlee9096@gmail.com>
2018-12-26 02:58:46 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,2 @@
 function Scooter(year, color, model) { this.year = year, this.color = color, this.model = model}
+function Driver(name, age, experience) { this.name = name, this.age = age, this.experience = experience}
